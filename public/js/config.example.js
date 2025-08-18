db15180ce4d8a7e3e42bc25b938f231cfda9fb35
iRilx7 <yamkr77@gmail.com>
2025-08-18 23:05:40 +0800

    Delete public directory

--- public/js/config.example.js
+++
@@ -1,2 +0,0 @@
-export const SUPABASE_URL = "https://YOUR-PROJECT.supabase.co";
-export const SUPABASE_ANON_KEY = "YOUR-ANON-PUBLIC-KEY";
--- public/js/config.js
+++
@@ -1,2 +0,0 @@
-export const SUPABASE_URL = "https://pfjbwjpgwscolqiqtyxt.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmamJ3anBnd3Njb2xxaXF0eXh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU0MTg5NDgsImV4cCI6MjA3MDk5NDk0OH0.D1H1O9E8pb_tzL4XXg_yC4yrbXwyuYwV5YEH-PXYVEo";
